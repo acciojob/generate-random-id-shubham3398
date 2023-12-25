4ec1f5fabe6b022b7547dd6e9bb43930f4270984
shubham3398 <117896201+shubham3398@users.noreply.github.com>
2023-12-25 15:28:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function makeid(l) {
   // write your code here
+	const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+	let ans = "";
+
+	for(let i = 0; i < l; i++){
+		let index = Math.floor((Math.random() * characters.length) % characters.length);
+		ans += characters.charAt(index);	
+	}
+
+	return ans;
 }
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
-alert(makeid(l));
+alert(makeid(l)); 
